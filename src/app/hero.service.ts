aaa31eed99bc01011523add5a2e41607d8f19bc8
filip <fchatys@gmail.com>
2022-10-24 21:01:54 +0200

    Step 4 adding services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { HEROES } from './mock-heroes';
+import {Hero} from "./Hero";
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
 
-  constructor() { }
+
+  constructor(private messageService: MessageService) { }
+
 }
+
+
+
+
+
