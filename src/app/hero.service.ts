17240a60306fe8deec5236c535510218ba5724ba
filip <fchatys@gmail.com>
2022-11-01 19:48:15 +0100

    Step 5 adding navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
 
-import { HEROES } from './mock-heroes';
-import {Hero} from "./Hero";
 import { Observable, of } from 'rxjs';
-import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import {Hero} from "./Hero";
+
+@Injectable({ providedIn: 'root' })
 export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-
-  constructor(private messageService: MessageService) { }
-
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
-
 
 
 
